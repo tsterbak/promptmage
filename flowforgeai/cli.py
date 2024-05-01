75b5070caec52a154aac8e7cf7c774c18523fe46
Tobias Sterbak <github@tobiassterbak.com>
2024-05-01 16:10:57 +0200

    Update flow discovery

--- flowforgeai/cli.py
+++ flowforgeai/cli.py
@@ -1,26 +1,46 @@
-"""This module contains the command line interface for the flowforge package."""
+"""This module contains the command line interface for the FlowForge package."""
 
+import click
 import uvicorn
 from importlib import import_module
 from pathlib import Path
 import sys
 
+from flowforgeai import FlowForge
 
-def serve(file_path: str):
-    # Dynamically import the application module
+
+@click.command()
+@click.argument("file_path", type=click.Path(exists=True))
+@click.option(
+    "--host", default="0.0.0.0", help="The host IP to run the FastAPI server on."
+)
+@click.option(
+    "--port", default=8000, type=int, help="The port to run the FastAPI server on."
+)
+def serve(file_path, host, port):
+    """Serve a FastAPI application containing a FlowForge instance from the given file."""
     sys.path.append(
         str(Path(file_path).parent.absolute())
     )  # Add the directory of the file to PYTHONPATH
     module_name = Path(file_path).stem
-    spec = import_module(module_name)
+    module = import_module(module_name)
 
-    # Assuming there is a FlowForge instance named `flow` in the module
-    flow = getattr(spec, "flow")
-    app = flow.serve()
+    # Find an instance of FlowForge in the module
+    flow_instance = None
+    for attr_name in dir(module):
+        attr = getattr(module, attr_name)
+        if isinstance(attr, FlowForge):
+            flow_instance = attr
+            break
+
+    if not flow_instance:
+        raise ValueError("No FlowForge instance found in the module.")
+
+    app = flow_instance.serve()
 
     # Run the FastAPI app
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=host, port=port)
 
 
 if __name__ == "__main__":
-    serve(sys.argv[1])
+    serve()
