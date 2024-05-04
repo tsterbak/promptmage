809081bd01a255699b4a2567cc656cd06ac1739d
Tobias Sterbak <github@tobiassterbak.com>
2024-05-04 13:25:56 +0200

    Enable frontend and api at the same time

--- flowforgeai/cli.py
+++ flowforgeai/cli.py
@@ -2,11 +2,25 @@
 
 import click
 import uvicorn
+import multiprocessing
 from importlib import import_module
 from pathlib import Path
 import sys
 
+from streamlit.web import cli as stcli
+
 from flowforgeai import FlowForge
+
+
+def run_fastapi(app, host, port):
+    # Run the FastAPI app
+    uvicorn.run(app, host=host, port=port)
+
+
+def run_streamlit():
+    # Run the Streamlit app
+    sys.argv = ["streamlit", "run", "flowforgeai/app.py"]
+    sys.exit(stcli.main())
 
 
 @click.command()
@@ -39,7 +53,19 @@ def serve(file_path, host, port):
     app = flow_instance.serve()
 
     # Run the FastAPI app
-    uvicorn.run(app, host=host, port=port)
+    fastapi_process = multiprocessing.Process(
+        target=run_fastapi, args=(app, host, port)
+    )
+    # Run the Streamlit app
+    streamlit_process = multiprocessing.Process(target=run_streamlit)
+
+    # Start both processes
+    fastapi_process.start()
+    streamlit_process.start()
+
+    # Wait for both processes to finish
+    fastapi_process.join()
+    streamlit_process.join()
 
 
 if __name__ == "__main__":
