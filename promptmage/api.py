1e548840e889b84e74dc7e73d3123f2539a09b7c
Tobias Sterbak <github@tobiassterbak.com>
2024-07-11 09:52:02 +0200

    Make multiflow approach nicely work with the api

--- promptmage/api.py
+++ promptmage/api.py
@@ -3,9 +3,10 @@
 import inspect
 import pkg_resources
 from loguru import logger
-from typing import List
+from typing import List, Callable
 from pathlib import Path
 from pydantic import BaseModel
+from slugify import slugify
 
 from fastapi import FastAPI, Path, Query
 from fastapi.staticfiles import StaticFiles
@@ -37,6 +38,13 @@ class PromptMageAPI:
             allow_headers=["*"],
         )
 
+        static_files_path = pkg_resources.resource_filename("promptmage", "static/")
+        app.mount(
+            "/static/",
+            StaticFiles(directory=static_files_path, html=True),
+            name="static",
+        )
+
         # create index endpoint
         @app.get("/")
         async def index():
@@ -47,63 +55,81 @@ class PromptMageAPI:
                 """<h2>To access the GUI 🧙, go to <a href='/gui'>/gui</a></h2>"""
             )
 
+        # create index API endpoint
         @app.get("/api")
         async def root():
-            return HTMLResponse("<h1>Welcome to the PromptMage API</h1>")
+            flow_names_to_slug = {flow.name: slugify(flow.name) for flow in self.flows}
+            flow_message = "<h2>Available flows:</h2>"
+            for name, slug in flow_names_to_slug.items():
+                flow_message += f"<br><a href='/api/{slug}'>{name}</a>"
+            return HTMLResponse(
+                f"<h1>Welcome to the PromptMage API</h1><p>{flow_message}</p>"
+            )
 
-        @app.get("/api/prompts")
-        async def list_prompts():
-            return self.mage.prompt_store.get_prompts()
+        # create an endpoint list of all available flows
+        @app.get("/api/flows")
+        async def list_flows():
+            flow_names_to_slug = {flow.name: slugify(flow.name) for flow in self.flows}
+            return flow_names_to_slug
 
-        @app.get("/api/data")
-        async def list_data():
-            return self.mage.data_store.get_all_data()
+        # create the endpoints for each flow
+        for flow in self.flows:
 
-        # create the endpoints for each step
-        step_list = []
-        for step_name, step in self.mage.steps.items():
-            signature = inspect.signature(step.func)
-            path = f"/api/{step_name}"
+            @app.get(f"/api/{slugify(flow.name)}/prompts", tags=[flow.name])
+            async def list_prompts():
+                return self.mage.prompt_store.get_prompts()
+
+            @app.get(f"/api/{slugify(flow.name)}/data", tags=[flow.name])
+            async def list_data():
+                return self.mage.data_store.get_all_data()
+
+            # add a route to list all available steps with their names and input variables
+            @app.get(f"/api/{slugify(flow.name)}/steps", tags=[flow.name])
+            async def list_steps():
+                return step_list
+
+            # create the endpoints for each step
+            step_list = []
+            for step_name, step in self.mage.steps.items():
+                signature = inspect.signature(step.func)
+                path = f"/api/{slugify(flow.name)}/{step_name}"
+                params, path_variables = self._parameters_from_signature(signature)
+                path += path_variables
+
+                # Update the signature for the endpoint function
+                new_signature = signature.replace(parameters=params)
+                endpoint_func = self.create_endpoint_function(step.execute)
+                setattr(
+                    endpoint_func, "__signature__", new_signature
+                )  # Update the signature for FastAPI to recognize
+
+                # Add the route to FastAPI
+                app.add_api_route(
+                    path,
+                    endpoint_func,
+                    methods=["GET"],
+                    tags=[flow.name],
+                    response_model=EndpointResponse,
+                )
+                step_list.append({"name": step_name, "path": path})
+
+            # create an endpoint to run the full dependency graph of the flow
+            run_function = self.mage.get_run_function()
+            signature = inspect.signature(run_function)
+            path = f"/api/{slugify(flow.name)}/run_flow"
             params, path_variables = self._parameters_from_signature(signature)
             path += path_variables
-
-            # Update the signature for the endpoint function
             new_signature = signature.replace(parameters=params)
-            endpoint_func = self.create_endpoint_function(step.func, new_signature)
-            setattr(
-                endpoint_func, "__signature__", new_signature
-            )  # Update the signature for FastAPI to recognize
-
-            # Add the route to FastAPI
+            endpoint_func = self.create_endpoint_function(run_function)
+            setattr(endpoint_func, "__signature__", new_signature)
             app.add_api_route(
-                path, endpoint_func, methods=["GET"], response_model=EndpointResponse
+                path,
+                endpoint_func,
+                methods=["GET"],
+                response_model=EndpointResponse,
+                tags=[flow.name],
             )
-            step_list.append({"name": step_name, "path": path})
 
-        # create an endpoint to run the full dependency graph of the flow
-        run_function = self.mage.get_run_function()
-        signature = inspect.signature(run_function)
-        path = "/api/run_flow"
-        params, path_variables = self._parameters_from_signature(signature)
-        path += path_variables
-        new_signature = signature.replace(parameters=params)
-        endpoint_func = self.create_endpoint_function(run_function, new_signature)
-        setattr(endpoint_func, "__signature__", new_signature)
-        app.add_api_route(
-            path, endpoint_func, methods=["GET"], response_model=EndpointResponse
-        )
-
-        # add a route to list all available steps with their names and input variables
-        @app.get("/api/steps")
-        async def list_steps():
-            return step_list
-
-        static_files_path = pkg_resources.resource_filename("promptmage", "static/")
-        app.mount(
-            "/static/",
-            StaticFiles(directory=static_files_path, html=True),
-            name="static",
-        )
         return app
 
     def _parameters_from_signature(self, signature):
@@ -135,7 +161,7 @@ class PromptMageAPI:
             params.append(new_param)
         return params, path
 
-    def create_endpoint_function(self, func, params):
+    def create_endpoint_function(self, func: Callable) -> Callable:
         # Define the endpoint function using dynamic parameters
         async def endpoint(*args, **kwargs):
             try:
